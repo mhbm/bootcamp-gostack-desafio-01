99ccc8e620a11c266d30b421ebf190c6b93ba47f
Mateus Macedo <mhbassomacedo@gmail.com>
2020-02-12 21:35:57 -0300

    Create middleware for check the index of projects
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- api/server.js
+++ api/server.js
@@ -6,6 +6,16 @@ server.use(express.json());
 
 let projects = [];
 
+function verifyProjectIdExist(request, response, next) {
+  console.log(request.params.id);
+  if (projects.length <= request.params.id) {
+    return response.status(400).json({
+      "message": "ProjectId does not exist"
+    })
+  }
+  next();
+}
+
 server.get("/projects", (request, response) => {
   console.log("Chamada do projets");
   return response.status(200).json(projects)
@@ -21,11 +31,20 @@ server.post("/projects", (request, response) => {
   return response.status(201).json(project);
 });
 
-server.put("/projects/:id", (request, response) => {
+server.put("/projects/:id", verifyProjectIdExist, (request, response) => {
   projects[request.params.id].title = request.body.title
 
   return response.status(200).json(projects[request.params.id]);
 });
 
+server.delete("/projects/:id", verifyProjectIdExist, (request, response) => {
+  projects.splice(request.params.id, 1);
+  return response.status(200).json(projects);
+});
+
+server.post("/projects/:id/tasks", verifyProjectIdExist, (request, response)=> {
+  projects[request.params.id].tasks.push(request.body.title);
+  return response.status(201).json(projects[request.params.id]);
+})
 
 server.listen(4444);
